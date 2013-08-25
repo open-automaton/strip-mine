9502481b2df3d4daa11439ca6cc5080b43b1827f
khrome <abbey@khrome.net>
2013-08-24 19:48:43 -0700

    updated excavate

--- excavate.js
+++ excavate.js
@@ -1,19 +1,3 @@
 #!/usr/bin/env node
 
-var Miner = request('./miner');
-
-process.on('message', function(message) {
-    switch(message.type){
-        var options = message.data || {};
-        case 'scrape' : 
-            var miner = new Miner(options);
-            miner.start(function(data){
-                process.send({
-                    type : 'scrape-return',
-                    data : data
-                });
-            });
-            break;
-        default : throw('unknown message type:'+message.type);
-    }
-});
+require('./strip-mine').waitForWork();
